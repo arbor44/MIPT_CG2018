8c912fabbe191eb500fded23d4c67efefda3e612
Borzov Artyom <borzov.artyom@phystech.edu>
2018-12-10 01:03:47 +0300

    fix bags in task2

--- task2/utils.py
+++ task2/utils.py
@@ -18,16 +18,16 @@ def convert_to_grayscale(im, weights=[0.3, 0.6, 0.1]):
     return gray
 
 
-def task2(im, method, threshold):
+def task2(im, method, threshold, max_inten=255):
     gray = im.copy()
-    gray = method(gray, threshold)
+    gray = method(gray, threshold, max_inten)
     plt.figure(figsize=(10, 13))
     plt.imshow(gray, cmap='gray')
 
 
-def apply_threshold(im, threshold):
+def apply_threshold(im, threshold, max_inten=255):
     idx = im > threshold
-    im[idx] = 255
+    im[idx] = max_inten
     im[np.logical_not(idx)] = 0
     return im
 
@@ -43,34 +43,34 @@ def ordered_threshold(size):
         return np.vstack([top, bottom])
 
 
-def forward_raw_process(im, threshold, raw, errors):
+def forward_raw_process(im, threshold, raw, errors, max_inten):
     raws = im[raw : raw + errors.shape[0]]
     
     for i in range(1, raws.shape[1]-1):
-        error = raws[0, i] - float(raws[0, i] > threshold) * 255
-        raws[0, i] = float(raws[0, i] > threshold) * 255
+        error = raws[0, i] - float(raws[0, i] > threshold) * max_inten
+        raws[0, i] = float(raws[0, i] > threshold) * max_inten
         matrix_error = error * errors
         start = math.ceil(errors.shape[1] / 2) - 1
         stop = math.floor(errors.shape[1] / 2) + 1
         raws[:, i - start : i + stop] = raws[:, i - start : i + stop] + matrix_error
-        raws[raws > 255] = 255
+        raws[raws > max_inten] = max_inten
         raws[raws < 0] = 0
 
     im[raw : raw + errors.shape[0], :] = raws
     
     return im
 
-def backward_raw_process(im, threshold, raw, errors):
+def backward_raw_process(im, threshold, raw, errors, max_inten):
     raws = im[raw : raw + errors.shape[0]]
     
     for i in range(raws.shape[1]-2, 0, -1):
-        error = raws[0, i] - float(raws[0, i] > threshold) * 255
-        raws[0, i] = float(raws[0, i] > threshold) * 255
+        error = raws[0, i] - float(raws[0, i] > threshold) * max_inten
+        raws[0, i] = float(raws[0, i] > threshold) * max_inten
         matrix_error = error * errors
         start =  math.floor(errors.shape[1] / 2)
         stop = math.ceil(errors.shape[1] / 2)
         raws[:, i - start : i + stop] = raws[:, i - start : i + stop] + matrix_error 
-        raws[raws > 255] = 255
+        raws[raws > max_inten] = max_inten
         raws[raws < 0] = 0
     
     im[raw : raw + errors.shape[0], :] = raws
@@ -85,47 +85,53 @@ def add_zeros(im):
     return img
 
 
-def error1_method(im, threshold=128):
+def error1_method(im, threshold=128, max_inten=255):
     img = im.copy()
     img = add_zeros(img)
     
     for raw in range(im.shape[0]):
-        img = forward_raw_process(img, threshold, raw, np.array([[0, 1]]))
+        img = forward_raw_process(img, threshold, raw, np.array([[0, 1]]), max_inten)
     
     return img
 
 
-def error2_method(im, threshold=128):
+def error2_method(im, threshold=128, max_inten=255):
     img = im.copy()
     img = add_zeros(img)
     
     for raw in range(im.shape[0]):
         if raw%2 == 0:
-            img = forward_raw_process(img, threshold, raw, np.array([[0, 1]]))
+            img = forward_raw_process(img, threshold, raw, np.array([[0, 1]]), max_inten)
         if raw%2 == 1:
-            img = backward_raw_process(img, threshold, raw, np.array([[1, 0]]))
+            img = backward_raw_process(img, threshold, raw, np.array([[1, 0]]), max_inten)
     
     return img
 
 
-def FS1_method(im, threshold=128):
+def FS1_method(im, threshold=128, max_inten=255):
     img = im.copy()
     img = add_zeros(img)
     
     for raw in range(im.shape[0]):
-        img = forward_raw_process(img, threshold, raw, np.array([[0, 0, 7/16], [3/16, 5/16, 1/16]]))
+        img = forward_raw_process(img, threshold, raw,
+                                  np.array([[0, 0, 7/16], [3/16, 5/16, 1/16]]),
+                                  max_inten)
     
     return img
 
 
-def FS2_method(im, threshold=128):
+def FS2_method(im, threshold=128, max_inten=255):
     img = im.copy()
     img = add_zeros(img)
     
     for raw in range(im.shape[0]):
         if raw%2 == 0:
-            img = forward_raw_process(img, threshold, raw, np.array([[0, 0, 7/16], [3/16, 5/16, 1/16]]))
+            img = forward_raw_process(img, threshold, raw,
+                                      np.array([[0, 0, 7/16], [3/16, 5/16, 1/16]]),
+                                      max_inten)
         if raw%2 == 1:
-            img = backward_raw_process(img, threshold, raw, np.array([[7/16, 0, 0], [1/16, 5/16, 3/16]]))
+            img = backward_raw_process(img, threshold, raw,
+                                       np.array([[7/16, 0, 0], [1/16, 5/16, 3/16]]),
+                                       max_inten)
     
     return img
